9917d30bf8890246e0569929812b3484359ef730
thtn-dev <thtn.1611.dev@gmail.com>
2025-04-07 23:04:43 +0700

    fix .env

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -7,11 +7,12 @@ from sqlalchemy import pool
 from alembic import context
 from sqlmodel import SQLModel
 from app.models import user_model
-from app.config import settings
+from app.config.settings import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -1,11 +1,11 @@
+import os
 from pydantic_settings import BaseSettings
 from typing import Optional
-
 class Settings(BaseSettings):
     DATABASE_URL: str 
-    
+    GOOGLE_API_KEY: Optional[str] = None
     class Config:
-        env_file = ".env"
+        env_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), ".env")
         env_file_encoding = "utf-8"
         
 settings = Settings()
