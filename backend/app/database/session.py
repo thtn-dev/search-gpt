0c71abb54c75099230b4197c4163fa1062ca072c
Tang Ho Trung Nam <thtn.1611.dev@gmail.com>
2025-04-18 09:49:51 +0000

    change pg driver

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -1,13 +1,34 @@
 from sqlmodel import create_engine, Session
 from typing import Generator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 from app.config.settings import settings
 
 DATABASE_URL = settings.DATABASE_URL
 
-engine = create_engine(
+engine = create_async_engine(
     DATABASE_URL,
+    echo=True,
+    future=True,
 )
+# Create a session factory
+async_session_factory = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+# Create a session generator
+async def get_async_session() -> Generator[AsyncSession, None, None]: # type: ignore
+    async with async_session_factory() as session:
+        yield session
+# Create a synchronous engine
 
-def get_db() -> Generator[Session, None, None]:
+sync_engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+    future=True,
+)
+# Create a synchronous session generator
+def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
